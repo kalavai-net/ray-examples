1fe572f317bbcbbbebe68fc2adb1e0fc9e197416
Adam Tomkins <artomkins@googlemail.com>
2024-01-07 18:22:31 +0000

    update model

--- serve/huggingface/llms/model.py
+++ serve/huggingface/llms/model.py
@@ -3,6 +3,7 @@ import pandas as pd
 from ray import serve
 from starlette.requests import Request
 
+import os
 
 @serve.deployment(ray_actor_options={"num_gpus": 1})
 class PredictDeployment:
@@ -14,8 +15,7 @@ class PredictDeployment:
             model_id,
             torch_dtype=torch.float16,
             low_cpu_mem_usage=True,
-            device_map="auto",  # automatically makes use of all GPUs available to the Actor
-            #load_in_8bit=True
+            device_map="auto"
         )
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -31,8 +31,15 @@ class PredictDeployment:
             temperature=0.9,
             max_length=100,
         )
+
+        generations = self.tokenizer.batch_decode(gen_tokens)
+
+        generations["responses"].append(
+            os.environ.get('MODEL_ID',"ENV not set")
+        )
+        
         return pd.DataFrame(
-            self.tokenizer.batch_decode(gen_tokens), columns=["responses"]
+            generations, columns=["responses"]
         )
 
     async def __call__(self, http_request: Request) -> str:
@@ -46,5 +53,7 @@ class PredictDeployment:
                 prompts.append(text)
         return self.generate(prompts)
 
-model_id = "facebook/opt-125m"
+model_id = os.environ.get('MODEL_ID',"facebook/opt-125m")
+print("Model ID", model_id)
+
 deployment = PredictDeployment.bind(model_id=model_id)
